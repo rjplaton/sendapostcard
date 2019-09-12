8daa7783973d1b9adb84de96d4d1e347e8ddc16c
thutran <thutran2611@yahoo.com>
2019-09-11 21:12:13 -0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link, Switch, Route } from 'react-router-dom'
-
+import logo from './images/logo/download.png';
 import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
@@ -12,9 +12,12 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="App-navigation">
-          <img src="download.png" alt="Icon"/>
+          <img src={logo} alt="Some logo"/>
           <h1 className="App-title">Send a postcard</h1>
         </nav>
+        <div className="App-intro">
+        <h2> Why leave your house to send a postcard? Leave it to us!</h2>
+        </div>
 
         <div className="App-mainContent">
 
