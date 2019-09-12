20f65812f2bb4adf356b65bf647369244810b6ae
Marisa Yeung <neurosa@gmail.com>
2019-09-12 14:18:00 -0700

    syncing up App files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-// import { Link, Switch, Route } from 'react-router-dom'
-
+import { Link, Switch, Route } from 'react-router-dom'
+import logo from './images/logo/download.png';
 import './App.css';
 
-// import LandingPage from './components/pages/LandingPage/LandingPage.js';
-// import Blog from './components/pages/Blog/Blog.js';
-// import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
-import Compose from './components/pages/Compose/Compose.js';
+import LandingPage from './components/pages/LandingPage/LandingPage.js';
+import Blog from './components/pages/Blog/Blog.js';
+import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
+import Compose from './components/Compose/Compose.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Compose />
+        <nav className="App-navigation">
+          <img src={logo} alt="Some logo"/>
+          <h1 className="App-title">Send a postcard</h1>
+        </nav>
+        <div className="App-intro">
+        <h2> Why leave your house to send a postcard? Leave it to us!</h2>
+        </div>
+
+        <div className="App-mainContent">
+
+        </div>
+
+        <Switch>
+          <Route exact path='/compose/' component={Compose}/>
+        </Switch>
+
       </div>
     );
   }
