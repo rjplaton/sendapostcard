148f2ce3466a08e29f7cdcc28e1156b6095fa045
Marisa Yeung <neurosa@gmail.com>
2019-09-24 01:25:07 -0700

    Added stripe checkout form, checkout isn't working yet

--- client/src/components/pages/Compose/Compose.js
+++ client/src/components/pages/Compose/Compose.js
@@ -18,11 +18,47 @@ class Compose extends Component {
             recZip: "00000",
         },
         showCheckout: false,
+        card_id: null,
     }
 
     checkout = (ev) => {
+        ev.preventDefault();
         console.log("show checkout form");
-        this.setState({showCheckout: true});
+
+        const formData = {
+            toAddress: {
+              name: this.state.formData.recName,
+              address_line1: this.state.formData.recAddress1,
+              address_line2: this.state.formData.recAddress2,
+              address_city: this.state.formData.recCity,
+              address_state: this.state.formData.recState,
+              address_zip: this.state.formData.recZip,
+              address_country: "US"
+            },
+            cardBack_text: this.state.formData.message,
+            frontTemplateId: this.state.front_template_id,
+            status: "saved",
+            stripeChargeId: null,
+            lobApiId: null,
+            createDate: new Date(),
+            lastModifiedDate: new Date(), //TODO: modify date when editing
+          };
+
+        fetch('/api/mongodb/sendapostcard/postcards', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Got this back', data);
+            console.log('Got this id back', data.results.insertedIds[0])
+
+            this.setState({
+                card_id: data.results.insertedIds[0],
+                showCheckout: true,
+            })
+        })
     }
 
     showMessageForm = () => {
@@ -87,6 +123,7 @@ class Compose extends Component {
                         showCheckout={this.state.showCheckout}
                         checkout={this.checkout}
                         showMessageForm={this.showMessageForm}
+                        card_id={this.state.card_id}
                     />
                 </div>
             </div>
