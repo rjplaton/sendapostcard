eeebae148451b4db0fab291dca50ba4b915ab1b5
Marisa Yeung <neurosa@gmail.com>
2019-09-24 23:57:23 -0700

    got lobcardid and thankyou page working

--- client/src/components/CheckoutForm/CheckoutForm.js
+++ client/src/components/CheckoutForm/CheckoutForm.js
@@ -11,7 +11,7 @@ class CheckoutForm extends Component {
         this.state = {
             complete: false,
             error: false,
-            lobApiId: null,
+            lobCardID: null,
         };
         this.submit = this.submit.bind(this);
     }
@@ -21,51 +21,32 @@ class CheckoutForm extends Component {
         let { token } = await this.props.stripe.createToken({ name: "Name" });
         console.log(token.id, this.props.card_id)
         //Adding props for card id to allow passing card_id
-        let response = await fetch(("/charge/" + this.props.card_id), {
+        await fetch("/charge_new/" + this.props.card_id, {
             method: "POST",
             headers: { "Content-Type": "text/plain" },
             body: token.id
-        });
-
-        if (response.ok) {
-            console.log("Purchase Complete!");
-            this.setState({ complete: true });
-
-            console.log('Fetching lob api id');
-
-            //hardcoding a delay 
-            //this should be updated to be more precise 
-            //i.e. only happen when the server.js send_postcard finishes successfully
-            setTimeout(
-                function () {
-                    fetch('/getLobApiId/' + this.props.card_id)
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log('Got data back', data);
-                            this.setState({
-                                lobApiId: data.lobApiId,
-                            })
-                        })
-
-                }.bind(this),
-                5000
-            );
-
-        } else {
-            this.setState({ error: true })
-            console.log("Charge failed.")
-        }
+        }).then(response => response.json())
+        .then(data => {
+            console.log("LALA data", data);
+            this.setState({lobCardID: data.postcard});
+        })
     }
 
     render() {
-        if (this.state.lobApiId) return <Redirect to={'/thank-you/' + this.state.lobApiId} />;
+        if (this.state.lobCardID) {
+            console.log(this.state.lobCardID);
+            // return (
+            //     <div><h4>Thank you!</h4></div>
+            // )
+            return <Redirect to={'/thank-you/' + this.state.lobCardID} />;
+        }
         if (this.state.error)
             return <div><h4>Something went wrong.</h4><p>You were not charged.</p><p>Please refresh the page and try again.</p></div>;
         return (
             <div className="CheckoutForm">
                 <h4>Payment Info</h4>
                 <div className="CheckoutForm-stripe">
-                    <CardElement style={{base: {fontSize: '18px'}}}/>
+                    <CardElement style={{ base: { fontSize: '18px' } }} />
                 </div>
                 {/* <h4>Order Details</h4> */}
                 <div className="CheckoutForm-total">
